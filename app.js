62f0ff3744e3697c84b5167c098c3904f70bb7d1
Omar Rodriguez <omar.rguez26@gmail.com>
2022-07-03 19:04:33 -0400

    added additional data to template

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const inquirer = require('inquirer');
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// const generatePage = require("./src/page-template.js");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("index.html", pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-
-// })
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -141,4 +132,13 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then (promptProject)
-    .then(portfolioData => console.log(portfolioData));
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile("index.html", pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log("Portfolio complete! Check out index.html to see the output!");
+
+        });
+    });
